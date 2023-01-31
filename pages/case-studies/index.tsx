b5b74bb33c135750a3cd2973c8d25d5a8c0cbf19
Moenen Erbuer <moenen@shapish.com>
2023-01-31 13:33:35 -0500

    x

--- pages/case-studies/index.tsx
+++ pages/case-studies/index.tsx
@@ -1,27 +1,31 @@
-import type { NextPage } from "next";
-import { useState } from "react";
-import { HighlyAvailable, ChooseHowToGetStarted, Security_02 } from "@carbon/pictograms-react";
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import {
+	HighlyAvailable,
+	ChooseHowToGetStarted,
+	Security_02,
+} from '@carbon/pictograms-react'
 // import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
-import Hero from "components/Hero";
-import Head from "components/Head";
-import Callout, { CalloutBody, CalloutHeading } from "components/Callout";
-import Lightbox from "components/Lightbox";
-import { FeatureList, FeatureListFeature } from "components/FeatureList";
-import LearnMore, { LearnMoreCard } from "components/LearnMore";
-import FormSignUp from "components/FormSignUp";
+import Hero from 'components/Hero'
+import Head from 'components/Head'
+import Callout, { CalloutBody, CalloutHeading } from 'components/Callout'
+import Lightbox from 'components/Lightbox'
+import { FeatureList, FeatureListFeature } from 'components/FeatureList'
+import LearnMore, { LearnMoreCard } from 'components/LearnMore'
+import FormSignUp from 'components/FormSignUp'
 
-import heroImage from "media/979477161_a_surface_under_an_electron_microscope__photorealistic__matte_painting__colorful.png";
-import videoSrc from "media/codeflare.mp4";
+import heroImage from 'media/979477161_a_surface_under_an_electron_microscope__photorealistic__matte_painting__colorful.png'
+import videoSrc from 'media/codeflare.mp4'
 
 const Home: NextPage = () => {
-  const [lightboxOpen, setLightboxOpen] = useState(false);
-  return (
-    <>
-      <Head />
-      <Hero src={heroImage} />
-      {/* <Hero setLightboxOpen={setLightboxOpen} src={heroImage} /> */}
-      {/* <Lightbox open={lightboxOpen} setOpen={setLightboxOpen}>
+	const [lightboxOpen, setLightboxOpen] = useState(false)
+	return (
+		<>
+			<Head />
+			<Hero src={heroImage} />
+			{/* <Hero setLightboxOpen={setLightboxOpen} src={heroImage} /> */}
+			{/* <Lightbox open={lightboxOpen} setOpen={setLightboxOpen}>
         <video
           onEnded={() => setLightboxOpen(false)}
           onPlay={() => {
@@ -38,36 +42,48 @@ const Home: NextPage = () => {
         </video>
       </Lightbox> */}
 
-      <Callout background="#333333" color="#ffffff" leftText="Case Studies">
-        {/* <CalloutHeading>
+			<Callout
+				background='#333333'
+				color='#ffffff'
+				leftText='Case Studies'
+			>
+				{/* <CalloutHeading>
           Learn how these early adopters use the Discovery Platform.
         </CalloutHeading> */}
-        <CalloutBody>
-          <LearnMore>
-            <LearnMoreCard href="/case-studies/drug-discovery">
-               Intelligent target-to-lead discovery of new drugs
-            </LearnMoreCard>
-            <LearnMoreCard href="/case-studies/materials-discovery">
-              Collaborative use of generative and foundation models for materials discovery
-            </LearnMoreCard>
-            <LearnMoreCard href="/case-studies/patient-monitoring">
-               Secure data access for digital health research
-            </LearnMoreCard>
-          </LearnMore>
-        </CalloutBody>
-      </Callout>
+				<CalloutBody>
+					<LearnMore>
+						<LearnMoreCard href='/case-studies/drug-discovery'>
+							Intelligent target-to-lead discovery of new drugs
+						</LearnMoreCard>
+						<LearnMoreCard href='/case-studies/materials-discovery'>
+							Collaborative use of generative and foundation
+							models for materials discovery
+						</LearnMoreCard>
+						<LearnMoreCard href='/case-studies/patient-monitoring'>
+							Secure data access for digital health research
+						</LearnMoreCard>
+					</LearnMore>
+				</CalloutBody>
+			</Callout>
 
-      <Callout id="signup" background="#a7f0ba" color="#044317" leftText="Sign Up">
-        <CalloutHeading>
-          Sign up for the SciDK to run scientific workflows on the Discovery Platform today and get access to new features as they roll out.
-        </CalloutHeading>
-        <CalloutBody>
-          <div id="signup-form"></div>
-          <FormSignUp />
-        </CalloutBody>
-      </Callout>
-    </>
-  );
-};
+			<Callout
+				id='signup'
+				background='#a7f0ba'
+				color='#044317'
+				leftText='Sign Up'
+			>
+				<CalloutHeading>
+					Sign up for the SciDK to run scientific workflows on the
+					Discovery Platform today and get access to new features as
+					they roll out.
+				</CalloutHeading>
+				<CalloutBody>
+					<div id='signup-form'></div>
+					<FormSignUp />
+				</CalloutBody>
+			</Callout>
+		</>
+	)
+}
 
-export default Home;
+export default Home
